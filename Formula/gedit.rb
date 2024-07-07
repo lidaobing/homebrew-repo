8c959721733429aebd249b61d3b3af9b656ca737
LI Daobing <lidaobing@gmail.com>
2024-07-07 14:15:01 -0700

    add gedit

---
+++ Formula/gedit.rb
@@ -0,0 +1,76 @@
+class Gedit < Formula
+  desc "GNOME text editor"
+  homepage "https://wiki.gnome.org/Apps/Gedit"
+  url "https://download.gnome.org/sources/gedit/47/gedit-47.0.tar.xz"
+  sha256 "fa4a597e34c76d4ac91431b5ae6e191c96c6b0af1702e0899ab35199edfa1bfa"
+  license "GPL-2.0-or-later"
+
+  # gedit doesn't seem to follow the typical GNOME version scheme, so we
+  # provide a regex to disable the `Gnome` strategy's version filtering.
+  livecheck do
+    url :stable
+    regex(/gedit[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "desktop-file-utils" => :build # for update-desktop-database
+  depends_on "docbook-xsl" => :build
+  depends_on "gtk-doc" => :build
+  depends_on "itstool" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "vala" => :build
+  depends_on "adwaita-icon-theme"
+  depends_on "at-spi2-core"
+  depends_on "cairo"
+  depends_on "gdk-pixbuf"
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "gobject-introspection"
+  depends_on "gsettings-desktop-schemas"
+  depends_on "gspell"
+  depends_on "gtk+3"
+  depends_on "libgedit-amtk"
+  depends_on "lidaobing/repo/libgedit-gtksourceview"
+  depends_on "lidaobing/repo/libgedit-tepl"
+  depends_on "libpeas@1"
+  depends_on "libsoup"
+  depends_on "libxml2"
+  depends_on "pango"
+
+  def install
+    ENV["DESTDIR"] = "/"
+    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+    ENV.append "LDFLAGS", "-Wl,-rpath,#{lib}/gedit" if OS.linux?
+
+    system "meson", "setup", "build", "-Dheaderbar=yes", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+  end
+
+  def post_install
+    system Formula["glib"].opt_bin/"glib-compile-schemas", HOMEBREW_PREFIX/"share/glib-2.0/schemas"
+    system Formula["gtk+3"].opt_bin/"gtk3-update-icon-cache", "-qtf", HOMEBREW_PREFIX/"share/icons/hicolor"
+  end
+
+  test do
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["icu4c"].opt_lib/"pkgconfig" if OS.mac?
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libpeas@1"].opt_lib/"pkgconfig"
+
+    # main executable test
+    system bin/"gedit", "--version"
+    # API test
+    (testpath/"test.c").write <<~EOS
+      #include <gedit/gedit-debug.h>
+
+      int main(int argc, char *argv[]) {
+        gedit_debug_init();
+        return 0;
+      }
+    EOS
+    flags = shell_output("pkg-config --cflags --libs gedit").chomp.split
+    flags << "-Wl,-rpath,#{lib}/gedit" if OS.linux?
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
