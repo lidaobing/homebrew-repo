e3e2ea133fe88ab4842d352e5784870a983818f6
LI Daobing <lidaobing@gmail.com>
2024-07-08 19:53:09 -0700

    fix head version

--- Formula/libgedit-gtksourceview.rb
+++ Formula/libgedit-gtksourceview.rb
@@ -4,7 +4,7 @@ class LibgeditGtksourceview < Formula
   url "https://github.com/gedit-technology/libgedit-gtksourceview/releases/download/299.2.1/libgedit-gtksourceview-299.2.1.tar.xz"
   sha256 "f94ea579636d73b4a783b9ec43d77bc9a43d4b633b3bf9ba9d7a011cadb5cb92"
   license "LGPL-2.1-only"
-  head "https://github.com/gedit-technology/libgedit-gtksourceview.git", branch: "main"
+  head "https://gitlab.gnome.org/World/gedit/libgedit-gtksourceview.git", branch: "main"
 
   # Upstream creates releases that use a stable tag (e.g., `v1.2.3`) but are
   # labeled as "pre-release" on GitHub before the version is released, so it's
